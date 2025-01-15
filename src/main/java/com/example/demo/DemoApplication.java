69fcb3cb19a6310cc8f2d88fc523dff952d467a6
Seowoo <92767091+5w31892p@users.noreply.github.com>
2025-01-15 18:35:35 +0900

    [refactor] Changing encryption algorithm and applying encryption

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing // JPA 엔티티 클래스에 대한 자동 감사(Auditing) 기능을 활성화하기 위해 사용
+@EnableEncryptableProperties // Jasypt 복호화
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/common/config/JasyptConfig.java
+++ src/main/java/com/example/demo/common/config/JasyptConfig.java
@@ -18,10 +18,11 @@ public class JasyptConfig {
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(encryptKey); // 암호화 키
         config.setAlgorithm("PBEWithHMACSHA512AndAES_256"); // 강력한 암호화 알고리즘 (기존 PBEWithMD5AndDES)
-        config.setKeyObtentionIterations("20000"); // 반복 해싱 회수 증가 (기존 1000)
-        config.setPoolSize("4"); // 병렬 처리 풀 크기 (기존 1)
+        config.setKeyObtentionIterations("1000"); // 반복 해싱 회수 증가 (기존 1000)
+        config.setPoolSize("1"); // 병렬 처리 풀 크기 (기존 1)
         config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스 default
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
         config.setStringOutputType("base64");
         encryptor.setConfig(config);
         return encryptor;
